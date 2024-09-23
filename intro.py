8153fe0eba268253960d13efe49b7e546437778e
MdaBest <makaylee.elizondo-koski@d11.org>
2024-09-23 11:28:31 -0600

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,37 +1,39 @@
 # 1. Print "Hello World!" to the console
-
+print ("Hello World!")
 
 
 # 2. Print "Hello my name is ______!" to the console
-
+print ("Hello my name is Makaylee!")
 
 
 # 3. Print "I love python!" to the console
-
+print ("I love pyhthon!")
 
 
 # 4. Print your age to the console
-
+print(18)
 
 
 # 5. Print 5+5
-
+print (5+5)
 
 
 # 6. Print 4*3
-
+print (4*3)
 
 
 # 7. Print 50 - 40
-
+print (50-40)
 
 
 # 8. Print 100/5 and print 100//5. Make a comment explaining what the difference is.
-
-
+print (100/5) 
+print (100//5)
+# its silly like that
 
 # 9. Print 2**4
-
+print (2**4)
 
 
 # 10. Print 6*3-4+8//2
+print (6*3-4+8//2)
